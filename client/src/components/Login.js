8f8f0285445fdf3214c9605ff44cfd2c01072d5b
K.Geetha Uma Maheswari <143368865+geethauma815@users.noreply.github.com>
2025-07-27 17:54:20 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post("https://placement-application-1.onrender.com/api/auth/login", form);
       localStorage.setItem("user", JSON.stringify(res.data.user));
       setMsg("✅ Login successful");
       navigate("/dashboard");
