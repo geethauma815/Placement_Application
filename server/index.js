4c4f9a1d96663d5db0cf443e09e5cb83bd7997e7
K.Geetha Uma Maheswari <143368865+geethauma815@users.noreply.github.com>
2025-07-28 22:26:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    // --- TEMPORARY DEBUG LOG ---
+    console.log("Request Origin:", origin); 
+
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
